e0995cdf2bbbb0ea98340ba0a7cbad2e60b9393c
MaxHearnden <maxoscarhearnden@gmail.com>
2023-08-27 00:29:42 +0100

    remove some dead code

--- src/runtime/scheduler/multi_thread/park.rs
+++ src/runtime/scheduler/multi_thread/park.rs
@@ -1,27 +1,19 @@
-use std::sync::atomic::AtomicUsize;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::{Arc, Condvar};
 use crate::util::TryLock;
-use crate::park::{Park, Unpark};
+use crate::park::Park;
 use crate::runtime::driver::Driver;
 
 pub struct Parker {
     inner: Arc<Inner>,
 }
 
-pub struct Unparker {
-    inner: Arc<Inner>,
-}
-
 struct Inner {
-    state: AtomicUsize,
-    mutex: Mutex<()>,
     condvar: Condvar,
     shared: Arc<Shared>,
 }
 
 struct Shared {
     driver: TryLock<Driver>,
-    handle: <Driver as Park>::Unpark,
 }
 
 impl Parker {
